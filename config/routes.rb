b50203d4eb8ff4e93c70c2585ec99569530447f3
Ezra Cunningham <ezz@vellz.world>
2024-09-24 16:56:44 +0100

    add update task feature

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
 end
